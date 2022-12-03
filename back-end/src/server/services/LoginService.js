fdea0e12abfb9b0a5a42f79b567ff0cd2b951839
vtrtga <vitor.ypvalim@gmail.com>
2022-12-03 14:58:55 -0400

    atualizando regras da rota POST /user, GET /user e testes

--- back-end/src/server/services/LoginService.js
+++ back-end/src/server/services/LoginService.js
@@ -5,10 +5,10 @@ const { comparePass } = require('../validations/md5Login');
 
 const login = async ({ email, password }) => {
   const user = await User.findOne({ where: { email } });
-  if (!user) throw errorGenerate(404, 'User not found');
+  if (!user) throw errorGenerate(404, 'Incorrect email or password');
 
   const compare = comparePass(password, user.password);
-  if (!compare) throw new Error('The password does not match');
+  if (!compare) throw new Error('Incorrect email or password');
   
   const { id, role } = user;
   const token = generateToken({ id, role });
