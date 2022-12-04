6c326af56267945906feba454aab260ed6f9c527
vtrtga <vitor.ypvalim@gmail.com>
2022-12-04 14:32:21 -0400

    set default role

--- back-end/src/server/validations/JoiSchemas.js
+++ back-end/src/server/validations/JoiSchemas.js
@@ -9,7 +9,6 @@ const registrationSchema = Joi.object({
   name: Joi.string().min(12).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
-  role: Joi.string().required(),
 });
 
 module.exports = { loginSchema, registrationSchema };
