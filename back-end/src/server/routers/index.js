5c3a7fe4b29815d03aa77dd0aa2b3975b81eca21
vtrtga <vitor.ypvalim@gmail.com>
2022-12-05 14:13:31 -0400

    getAll products route and tests

--- back-end/src/server/routers/index.js
+++ back-end/src/server/routers/index.js
@@ -1,7 +1,9 @@
 const loginRouter = require('./login');
 const userRouter = require('./user');
+const productRouter = require('./product');
 
 module.exports = {
   loginRouter,
   userRouter,
+  productRouter,
 };
