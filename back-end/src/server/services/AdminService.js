db93a766863ba63bccab57934ed3bc38898e3957
Gabriel <gabrielja2antunes@gmail.com>
2022-12-07 22:13:16 -0300

    avanço

--- back-end/src/server/services/AdminService.js
+++ back-end/src/server/services/AdminService.js
@@ -3,7 +3,7 @@ const { User } = require('../../database/models');
 const errorGenerate = require('../utils/errorGenerate');
 
 const create = async ({ name, email, password, role = 'customer' }) => {
-  const checkUser = await User.findOne({ where: { email } });
+  const checkUser = await User.findOne({ where: { email, name } });
   
   if (checkUser) throw errorGenerate(409, 'User already exists');
 
