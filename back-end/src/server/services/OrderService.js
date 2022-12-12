35d4c062a023c55567cd21748fe5c8d49745f6c3
Gabriel <gabrielja2antunes@gmail.com>
2022-12-12 19:28:06 -0300

    req 17 finalizado

--- back-end/src/server/services/OrderService.js
+++ back-end/src/server/services/OrderService.js
@@ -1,4 +1,4 @@
-const { Sale } = require('../../database/models');
+const { User, Sale } = require('../../database/models');
 
 const create = async ({ orderData, _productData }) => {
   const orders = await Sale.create(orderData);
@@ -11,7 +11,15 @@ const getAll = async (req) => {
   const orders = await Sale.findAll({ where: { id } });
   return orders;
 };
+
+const getAllSellers = async () => {
+  const sellers = await User.findAll({ where: { role: 'seller' }, attributes: ['id', 'name'] });
+
+  return sellers;
+};
+
 module.exports = {
   getAll,
   create,
+  getAllSellers,
 };
